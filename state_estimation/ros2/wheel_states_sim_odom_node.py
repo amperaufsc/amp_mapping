05d5f8c7e4f812d3e10912f5da9ce9fabd0187cd
possamai <pedrompossamai@gmail.com>
2026-04-24 16:29:20 -0300

    feat: creating src directory and wheel_odometry to make a more modular code

--- state_estimation/ros2/wheel_states_sim_odom_node.py
+++ state_estimation/ros2/wheel_states_sim_odom_node.py
@@ -5,8 +5,7 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu
 from fs_msgs.msg import WheelStates
 from message_filters import Subscriber, ApproximateTimeSynchronizer
-import numpy as np
-from scipy.spatial.transform import Rotation as R
+from src.wheel_odometry import WheelOdometry
 
 class WheelRpmSimOdom(Node):
     def __init__(self):
@@ -22,78 +21,13 @@ class WheelRpmSimOdom(Node):
         self.time_sync = ApproximateTimeSynchronizer([self.wheel_sub, self.imu_sub], queue_size, max_delay)
         self.time_sync.registerCallback(self.wheel_callback)
 
-        self.R = 0.18      # wheel radius
-        self.L = 1.6       # wheelbase distance
-        self.x = 0.0
-        self.y = 0.0
-        self.yaw = 0.0
-
-        self.last_time = None
+        self.wheel_odom = WheelOdometry()
 
     def wheel_callback(self, wheel_msg, imu_msg):
-        now = wheel_msg.header.stamp.sec + wheel_msg.header.stamp.nanosec * 1e-9
+        odom = self.wheel_odom.wheelStatesToOdom(wheel_msg, imu_msg)
 
-        if self.last_time is None:
-            self.last_time = now
-            return
-
-        dt = now - self.last_time
-        self.last_time = now
-
-        rpm_mean = (wheel_msg.fl_rpm + wheel_msg.fr_rpm + wheel_msg.rl_rpm + wheel_msg.rr_rpm) / 4.0
-        vx = rpm_mean * 2 * np.pi * self.R / 60.0
-
-        delta = (wheel_msg.fl_steering_angle + wheel_msg.fr_steering_angle) / 2.0
-        
-        wz = vx / self.L * np.tan(delta)
-        
-        vy = 0.0
-
-        #self.yaw += wz * dt
-
-        r = R.from_quat([imu_msg.orientation.x, imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w])
-        roll, pitch, yaw = r.as_euler('xyz', degrees=False)
-        self.yaw = yaw
-
-        self.x += vx * np.cos(self.yaw) * dt
-        self.y += vx * np.sin(self.yaw) * dt
-
-        odom = Odometry()
-        odom.header = wheel_msg.header
-        odom.header.frame_id = "fsds/odom"
-        odom.child_frame_id = "fsds/FSCar"
-
-        odom.pose.pose.position.x = self.x
-        odom.pose.pose.position.y = self.y
-
-        #transforming yaw to quaternion
-        
-        odom.pose.pose.orientation.z = imu_msg.orientation.z
-        odom.pose.pose.orientation.w = imu_msg.orientation.w
-
-        odom.twist.twist.linear.x = vx
-        odom.twist.twist.linear.y = vy
-        odom.twist.twist.angular.z = wz
-
-        odom.pose.covariance = [
-            1.0,   0.0,   0.0,   0.0,   0.0,   0.0,
-            0.0,   1.0,   0.0,   0.0,   0.0,   0.0,
-            0.0,   0.0,   1e6,   0.0,   0.0,   0.0,
-            0.0,   0.0,   0.0,   1e6,   0.0,   0.0,
-            0.0,   0.0,   0.0,   0.0,   1e6,   0.0,
-            0.0,   0.0,   0.0,   0.0,   0.0,   0.1
-        ]
-
-        odom.twist.covariance = [
-            0.02,  0.0,   0.0,   0.0,   0.0,   0.0,
-            0.0,   0.1,   0.0,   0.0,   0.0,   0.0,
-            0.0,   0.0,   1e6,   0.0,   0.0,   0.0,
-            0.0,   0.0,   0.0,   1e6,   0.0,   0.0,
-            0.0,   0.0,   0.0,   0.0,   1e6,   0.0,
-            0.0,   0.0,   0.0,   0.0,   0.0,   0.5
-        ]
-
-        self.odom_pub.publish(odom)
+        if odom is not None:
+            self.odom_pub.publish(odom)
 
 def main(args=None):
     rclpy.init()
---
+++ state_estimation/src/wheel_odometry.py
@@ -0,0 +1,78 @@
+from nav_msgs.msg import Odometry
+from scipy.spatial.transform import Rotation as R
+import numpy as np
+
+class WheelOdometry:
+    def __init__(self):
+        self.R = 0.18      # wheel radius
+        self.L = 1.6       # wheelbase distance
+        self.x = 0.0
+        self.y = 0.0
+        self.yaw = 0.0
+
+        self.last_time = None
+
+    def wheelStatesToOdom(self, wheel_msg, imu_msg):
+        now = wheel_msg.header.stamp.sec + wheel_msg.header.stamp.nanosec * 1e-9
+
+        if self.last_time is None:
+            self.last_time = now
+            return
+
+        dt = now - self.last_time
+        self.last_time = now
+
+        rpm_mean = (wheel_msg.fl_rpm + wheel_msg.fr_rpm + wheel_msg.rl_rpm + wheel_msg.rr_rpm) / 4.0
+        vx = rpm_mean * 2 * np.pi * self.R / 60.0
+
+        delta = (wheel_msg.fl_steering_angle + wheel_msg.fr_steering_angle) / 2.0
+        
+        wz = vx / self.L * np.tan(delta)
+        
+        vy = 0.0
+
+        #self.yaw += wz * dt
+
+        r = R.from_quat([imu_msg.orientation.x, imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w])
+        roll, pitch, yaw = r.as_euler('xyz', degrees=False)
+        self.yaw = yaw
+
+        self.x += vx * np.cos(self.yaw) * dt
+        self.y += vx * np.sin(self.yaw) * dt
+
+        odom = Odometry()
+        odom.header = wheel_msg.header
+        odom.header.frame_id = "fsds/odom"
+        odom.child_frame_id = "fsds/FSCar"
+
+        odom.pose.pose.position.x = self.x
+        odom.pose.pose.position.y = self.y
+
+        #transforming yaw to quaternion
+        
+        odom.pose.pose.orientation.z = imu_msg.orientation.z
+        odom.pose.pose.orientation.w = imu_msg.orientation.w
+
+        odom.twist.twist.linear.x = vx
+        odom.twist.twist.linear.y = vy
+        odom.twist.twist.angular.z = wz
+
+        odom.pose.covariance = [
+            1.0,   0.0,   0.0,   0.0,   0.0,   0.0,
+            0.0,   1.0,   0.0,   0.0,   0.0,   0.0,
+            0.0,   0.0,   1e6,   0.0,   0.0,   0.0,
+            0.0,   0.0,   0.0,   1e6,   0.0,   0.0,
+            0.0,   0.0,   0.0,   0.0,   1e6,   0.0,
+            0.0,   0.0,   0.0,   0.0,   0.0,   0.1
+        ]
+
+        odom.twist.covariance = [
+            0.02,  0.0,   0.0,   0.0,   0.0,   0.0,
+            0.0,   0.1,   0.0,   0.0,   0.0,   0.0,
+            0.0,   0.0,   1e6,   0.0,   0.0,   0.0,
+            0.0,   0.0,   0.0,   1e6,   0.0,   0.0,
+            0.0,   0.0,   0.0,   0.0,   1e6,   0.0,
+            0.0,   0.0,   0.0,   0.0,   0.0,   0.5
+        ]
+        
+        return odom
